9074c30e2842272bbb3cceff221e276ecb0ce023
정연재 <76952340+YeonjaeMan@users.noreply.github.com>
2024-11-16 15:11:43 +0900

    first commit

---
+++ src/main/java/com/gwangju/central_office_100/CentralOffice100Application.java
@@ -0,0 +1,13 @@
+package com.gwangju.central_office_100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralOffice100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralOffice100Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gwangju/central_office_100/CentralOffice100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwangju.central_office_100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralOffice100ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
